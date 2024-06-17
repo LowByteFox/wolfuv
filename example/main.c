9ce95ff74d7d853edff5e86c0b2a7bc457324962
LowByteFox <firethefox77@gmail.com>
2024-06-17 08:43:28 +0200

    improve api declarations and add echo example that will be used for testing

---
+++ example/main.c
@@ -0,0 +1,66 @@
+#include <uv.h>
+#include <wolfuv.h>
+
+uv_loop_t *loop;
+
+void alloc(uv_handle_t *handle, size_t suggested, uv_buf_t *buf) {
+    buf->base = malloc(suggested);
+    buf->len = suggested;
+}
+
+void echo_write(uv_write_t *req, int status) {
+    if (status) {
+        fprintf(stderr, "Write error %s\n", uv_strerror(status));
+    }
+    free(req);
+}
+
+void on_read(struct wuv_tcp *client, ssize_t nread, const uv_buf_t *buf) {
+    if (nread < 0) {
+        if (nread != UV_EOF) {
+            fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+            wuv_close(client, NULL);
+        }
+    } else if (nread > 0) {
+        uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t));
+        uv_buf_t wrbuf = uv_buf_init(buf->base, nread);
+        wuv_write(req, client, &wrbuf, 1, echo_write);
+    }
+
+    if (buf->base) {
+        free(buf->base);
+    }
+}
+
+void on_connection(struct wuv_tcp *server, int status) {
+    if (status < 0) {
+        fprintf(stderr, "New connection error %s\n", uv_strerror(status));
+        return;
+    }
+
+    struct wuv_tcp *client = malloc(sizeof(struct wuv_tcp));
+    wuv_tcp_init(loop, client);
+
+    if (wuv_accept(server, client) == 0) {
+        wuv_read_start(client, alloc, on_read);
+    } else {
+        wuv_close(client, NULL);
+    }
+}
+
+int main() {
+    wuv_init("./server-cert.pem", "./server-key.pem");
+    loop = uv_default_loop();
+
+    struct sockaddr_in addr;
+    struct wuv_tcp server = {0};
+    wuv_tcp_init(loop, &server);
+
+    uv_ip4_addr("0.0.0.0", 7000, &addr);
+
+    wuv_tcp_bind(&server, (const struct sockaddr*)&addr), 0);
+    wuv_tcp_listen(&server, 128, on_connection);
+
+    uv_run(loop, UV_RUN_DEFAULT);
+    return 0;
+}
--- include/wolfuv.h
+++ include/wolfuv.h
@@ -4,6 +4,11 @@
 #include <uv.h>
 #include <wolfssl/options.h>
 
+typedef void (*wuv_alloc_cb)(struct wuv_tcp*, size_t, uv_buf_t*);
+typedef void (*wuv_read_cb)(struct wuv_tcp*, ssize_t, const uv_buf_t*);
+typedef void (*wuv_connection_cb)(struct wuv_tcp*, int);
+typedef void (*wuv_close_cb)(struct wuv_tcp*);
+
 struct wuv_tcp {
         uv_loop_t *loop;
         uv_tcp_t tcp;
@@ -12,15 +17,19 @@ struct wuv_tcp {
         uv_os_fd_t fd;
 
         /* libuv callbacks */
-        uv_alloc_cb *alloc_cb;
-        uv_read_cb *read_cb;
+        wuv_alloc_cb *alloc_cb;
+        wuv_read_cb *read_cb;
 };
 
 int     wuv_init(const char *cert_path, const char *priv_path);
 void    wuv_deinit();
-int     wuv_listen(struct wuv_tcp*, int, uv_connection_cb);
-int     wuv_accept(struct uv_stream_t*, struct wuv_tcp*);
-int     wuv_close(struct wuv_tcp*, uv_close_cb);
+int     wuv_listen(struct wuv_tcp*, int, wuv_connection_cb);
+int     wuv_accept(struct wuv_tcp*, struct wuv_tcp*);
+int     wuv_close(struct wuv_tcp*, wuv_close_cb);
+int     wuv_read_start(struct wuv_tcp*, wuv_alloc_cb, wuv_read_cb);
+int     wuv_read_stop(struct wuv_tcp*);
+int     wuv_write(uv_write_t*, struct wuv_tcp*, uv_buf_t *, unsigned int,
+            uv_write_cb);
 
 int     wuv_tcp_init(uv_loop_t*, struct wuv_tcp*);
 int     wuv_tcp_bind(struct wuv_tcp*, const struct sockaddr*. unsigned int);
